c3b9141ffd8df959eae49981648897dedaf73c9f
The Milk Man <ajayseanx@gmail.com>
2022-09-26 14:21:59 +0100

    add themes

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,14 @@ import { Books } from "Pages/Books";
 import { Post } from "Pages/Post";
 import "./index.scss"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const getTheme = () => {
+  const currentMonth = new Date().getMonth() + 1;
+  return currentMonth === 12 ? "christmas" : currentMonth > 5 && currentMonth < 9 ? "summer" : "halloween";
+}
+
+const root = document.getElementById("root")!;
+root.classList.add(getTheme());
+ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
