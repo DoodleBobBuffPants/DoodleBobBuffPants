9ce57e96ec5a9ba688481a57bc3f4e9361c75ecd
The Milk Man <ajayseanx@gmail.com>
2023-03-07 18:15:46 +0000

    add lint rule to check importing React

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
   plugins: ["@typescript-eslint", "unused-imports", "react", "react-hooks"],
   rules: {
     eqeqeq: "error",
+    "no-restricted-syntax": [
+      "error",
+      { selector: "ImportDeclaration[source.value=react] ImportDefaultSpecifier[local.name=React]", message: "React should not be imported as automatic JSX imports are enabled" },
+    ],
     "unused-imports/no-unused-imports": "error",
     "react/no-array-index-key": "error",
     "react-hooks/rules-of-hooks": "error",
